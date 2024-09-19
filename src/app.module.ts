1e7e0a6dae79a1d1b58e824ff10553b4f88bd422
Indika Maligaspe <k.indika.maligaspe@gmail.com>
2024-09-19 22:16:38 +0400

    added configmodule to load env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import {MongooseModule} from '@nestjs/mongoose'
 
@@ -10,7 +11,16 @@ import {MongooseModule} from '@nestjs/mongoose'
   imports: [
     UserModule, 
     AuthModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/eg-db')],
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'), // Loaded from .ENV
+      })
+    }),
+  ],
+    // MongooseModule.forRoot('mongodb://localhost:27017/eg-db')],
   controllers: [AppController],
   providers: [AppService],
 })
